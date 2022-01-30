a1a943ba491f2a2d07e16bd9ea99a6ec3bfd3132
Ansu <Ansu@DESKTOP-5IO7C18>
2022-01-30 16:49:34 +0530

    Add project files.

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿
+using LoginRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginRegistration.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        LoginDbEntities db = new LoginDbEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveData(SiteUser model)
+        {
+            db.SiteUsers.Add(model);
+            db.SaveChanges();
+            return Json("Registration Successful", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
